7f6dbbee8459f0b759aab406d305c00072b7a81a
Adrian <acastillo@aasasoftglobal.com>
2025-12-15 15:39:40 -0500

    front que pegue al back

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
+//que el front detecte como api al back
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    proxy: { '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
